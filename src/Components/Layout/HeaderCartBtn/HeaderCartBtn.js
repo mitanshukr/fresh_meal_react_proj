f63601178a6b8a73492222b1c5fe73b702012b05
Mitanshu Kumar <mitanshukr@yahoo.com>
2021-08-06 13:21:40 +0530

    Added HeaderCartBtn Animation

--- src/Components/Layout/HeaderCartBtn/HeaderCartBtn.js
+++ src/Components/Layout/HeaderCartBtn/HeaderCartBtn.js
@@ -1,20 +1,41 @@
 import CartIcon from "./CartIcon";
 import style from "./HeaderCartBtn.module.css";
 import { cartContext } from "../../../ContextStore/context-store";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 
 const HeaderCartBtn = (props) => {
-const useCartContext = useContext(cartContext);
+  const useCartContext = useContext(cartContext);
+  const [bumpStyle, setBumpStyle] = useState(false);
 
+  const { items } = useCartContext;
 
-  const btnClasses = style.button + " " + style.bump;
+  const badgeCount = items.reduce((accum, item) => {
+    return accum + item.amount;
+  }, 0);
+
+  let btnClasses = `${style.button} ${bumpStyle ? style.bump : ""}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBumpStyle(true);
+    let timer = setTimeout(() => {
+      setBumpStyle(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
     <button className={btnClasses} onClick={props.onShowCart}>
       <span className={style.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={style.badge}>{useCartContext.items.length}</span>
+      <span className={style.badge}>{badgeCount}</span>
     </button>
   );
 };
